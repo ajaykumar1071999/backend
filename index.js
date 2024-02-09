ec8ed2458e9ed4bd7b380fb0b441b602ecd78810
Ajay Kumar <ajaykumar1071999@gmail.com>
2024-02-09 22:55:49 +0530

    JSON type added

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.get("/home",(req,res)=>{
-    res.send("This is home page.")
+app.get('/home', (req, res) => {
+  res.send('This is home page.');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Hii Ajay this is your first program</h1>');
+});
+
+app.get('/sendJson', (req, res) => {
+  res.json({ Id: 1, name: "Ajay Kumar" });
+});
+
+app.get("/error",(req,res)=>{
+    res.status(500).json({ error: 'message' })
 })
-app.get('/login',(req,res)=>{
-    res.send('<h1>Hii Ajay this is your first program</h1>')
-})
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-console.log("start from now")
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+console.log("start from now");
